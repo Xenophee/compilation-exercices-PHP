fbf9ef63191587249f6832655255d313d6047543
Xenophee <perrine.morsch@gmail.com>
2023-08-02 16:02:01 +0200

    ajout de la fonctionnalité pour donner les réponses

--- public/assets/js/calendar.js
+++ public/assets/js/calendar.js
@@ -11,3 +11,25 @@ const submitForm = () => {
 // création des ecouteurs
 month.addEventListener('change', submitForm);
 year.addEventListener('change', submitForm);
+
+
+// ======================================================================================
+// --------------------------------------------------------------------------------------
+const btn = document.querySelectorAll('.showCode');
+const modalImg = document.querySelector('.modal-img');
+const modal = document.querySelector('dialog');
+
+
+
+btn.forEach((element, key) => {
+    element.addEventListener('click', () => {
+        modal.showModal();
+
+        modalImg.src = `../public/assets/img/calendar/${key + 1}.png`;
+    });
+});
+
+
+modal.addEventListener('click', () => {
+    modal.close();
+});
