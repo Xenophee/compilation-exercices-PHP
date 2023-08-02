5221a260b2fd27a65d7fa5aee84b2b5931a21ccc
Xenophee <perrine.morsch@gmail.com>
2023-08-02 16:59:34 +0200

    rectification pour prendre en compte deux images dans l'affichage des réponses

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,15 +1,20 @@
 
 const btn = document.querySelectorAll('.showCode');
-const modalImg = document.querySelector('.modal-img');
+const modalImg = document.querySelectorAll('.modal-img');
 const modal = document.querySelector('dialog');
-
 
 
 btn.forEach((element, key) => {
     element.addEventListener('click', () => {
         modal.showModal();
 
-        modalImg.src = '../public/assets/img/form/' + (key +1) + '.png';
+        modalImg.forEach((element, key) => {
+            if (key === 0) {
+                element.src = `../public/assets/img/form/html/${key + 1}.png`;
+            } else {
+                element.src = `../public/assets/img/form/php/${key + 1}.png`;
+            }
+        });
     });
 });
 
