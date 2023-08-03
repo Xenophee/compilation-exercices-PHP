2993fba4026cb960c6ab960a0ea87ac1d79ae7b2
Xenophee <perrine.morsch@gmail.com>
2023-08-03 10:32:20 +0200

    ajout d'une fonction de formatage de date

--- helpers/functions.php
+++ helpers/functions.php
@@ -4,3 +4,16 @@
 function isActivePage($page, $currentPage) {
     return ($page == $currentPage) ? 'active' : '';
 }
+
+
+/**
+ * Permet de formater une date en français numérique et retourne le résultat
+ * @param string $date
+ * 
+ * @return string
+ */
+function dateFormat(string $date):string {
+    $date = new DateTime($date);
+    $date = $date->format('d/m/Y');
+    return $date;
+}
